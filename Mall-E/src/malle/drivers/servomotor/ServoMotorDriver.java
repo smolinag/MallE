a1e64b83b3ebceecf49f6b641c9d47889a52b647
smolinag <santiago.molina.g@gmail.com>
2019-08-21 07:35:00 -0500

    Added Ultrasound Driver

--- Mall-E/src/malle/drivers/servomotor/ServoMotorDriver.java
+++ Mall-E/src/malle/drivers/servomotor/ServoMotorDriver.java
@@ -6,6 +6,7 @@
 package malle.drivers.servomotor;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.wiringpi.*;
 
 /**
  *
@@ -15,29 +16,54 @@ public class ServoMotorDriver {
     
     private final int MIN_ANGLE_LIMIT = -90;
     private final int MAX_ANGLE_LIMIT = 90;
-    private final int START_ANGLE = 0;
-    private final int PWM_CLCK = 1920;
-    private final int PWM_RANGE = 200;
+    private final int PWM_CLCK = 192;
+    private final int PWM_RANGE = 2000;
     
-    private Integer minAngle;
-    private Integer maxAngle;
+    private final Integer minAngle;
+    private final Integer maxAngle;
+    private Integer currentAngle;
     
-    private RaspiPin pwmPin;
+    private Pin pwmPin;
+    private final GpioPinPwmOutput pwmPinOut;
     
-    public ServoMotorDriver(RaspiPin pwmPin, Integer minAngle, Integer maxAngle){
+    public ServoMotorDriver(Pin pwmPin, Integer minAngle, Integer maxAngle, Integer startAngle){
         this.pwmPin = pwmPin;
+        
+        //Configure PWM
+        GpioController gpio = GpioFactory.getInstance();        
+        pwmPinOut = gpio.provisionPwmOutputPin(pwmPin);
+        Gpio.pwmSetMode(Gpio.PWM_MODE_MS);
+        Gpio.pwmSetClock(PWM_CLCK);
+        Gpio.pwmSetRange(PWM_RANGE);
         
         //Set angle limits
         this.minAngle = minAngle != null && minAngle >= MIN_ANGLE_LIMIT && minAngle < MAX_ANGLE_LIMIT ? minAngle : MIN_ANGLE_LIMIT;
         this.maxAngle = maxAngle != null && maxAngle <= MAX_ANGLE_LIMIT && maxAngle > this.minAngle ? maxAngle : MAX_ANGLE_LIMIT;
         
         //Set initial position
-        
+        setAngle(startAngle);
     }
     
     public void increaseAngle(int angleStep){
-        
+        pwmPinOut.setPwm(angleToDutyCycle(currentAngle + angleStep));
     }
     
+    public void setAngle(int angleToSet){
+        pwmPinOut.setPwm(angleToDutyCycle(angleToSet));
+    }
     
+    private Integer angleToDutyCycle(int angle){
+        //Limit angle
+        if(angle < minAngle){
+            angle = minAngle;
+        }
+        if(angle > maxAngle){
+            angle = maxAngle;
+        }
+        currentAngle = angle;
+        
+        //Get dutycycle
+        double dutyCycleD = (50. / 90.) * angle + 150;         
+        return (int) Math.round(dutyCycleD);
+    }
 }
---
+++ Mall-E/src/malle/drivers/ultrasound/UltrasoundDriver.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package malle.drivers.ultrasound;
+
+import com.pi4j.io.gpio.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author smoli
+ */
+public class UltrasoundDriver {
+
+    private final Integer NUM_SAMPLES_FOR_SINGLE_MEASUREMENT = 5;
+
+    private final String sensorName;
+    private final Pin triggerPin;
+    private final Pin echoPin;
+    private final GpioPinDigitalOutput triggerOut;
+    private final GpioPinDigitalInput echoIn;
+
+    private boolean isSensing = false;
+    private Double distance;
+
+    public UltrasoundDriver(String sensorName, Pin trigger, Pin echo) {
+        GpioController gpio = GpioFactory.getInstance();
+
+        this.sensorName = sensorName;
+        triggerPin = trigger;
+        echoPin = echo;
+
+        triggerOut = gpio.provisionDigitalOutputPin(trigger);
+        echoIn = gpio.provisionDigitalInputPin(echo);
+    }
+
+    public synchronized Double getSingleDistanceMeasurement() {
+        if (!isSensing) {
+            List<Double> measurements = new ArrayList<>();
+            Thread ultrasoundThread = new Thread("ultrasound-" + sensorName) {
+                @Override
+                public void run() {
+                    try {
+                        //Discard first sample
+                        for (int i = 0; i < NUM_SAMPLES_FOR_SINGLE_MEASUREMENT; i++) {
+                            triggerOut.high();
+                            Thread.sleep((long) 0.01);// Delay for 10 microseconds
+                            triggerOut.low();
+
+                            //Wait until the ECHO pin gets HIGH
+                            while (echoIn.isLow()) {
+                            }
+                            // Store the surrent time to calculate ECHO pin HIGH time.
+                            long startTime = System.nanoTime(); 
+
+                            //Wait until the ECHO pin gets LOW
+                            while (echoIn.isHigh()) { 
+                            }
+                            // Store the echo pin HIGH end time to calculate ECHO pin HIGH time.
+                            long endTime = System.nanoTime(); 
+
+                            distance = (((endTime - startTime) / 1e3) / 2) / 29.1;
+                            measurements.add(distance);
+                            Thread.sleep(20);
+                        }                        
+                        distance = getMedianValue(measurements);
+                        isSensing = false;
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(UltrasoundDriver.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            ;
+            };
+            ultrasoundThread.start();
+            isSensing = true;
+        }
+        
+        while (isSensing) {
+        }
+        return distance;
+    }
+    
+    private Double getMedianValue(List<Double> measurements){
+        Collections.sort(measurements,(m1,m2) -> m1.compareTo(m2));
+        return measurements.get((int)Math.floor(NUM_SAMPLES_FOR_SINGLE_MEASUREMENT / 2.));
+    }
+}
